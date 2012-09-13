2b3b08674de475d24dcce7382eff8bc8dfcfeeb6
patil.ganesh170@gmail.com <webonise@webonise-desktop.(none)>
2012-09-13 16:39:37 +0530

    php assignment after completion of 10 questions

--- strAssgn.php
+++ strAssgn.php
@@ -28,6 +28,8 @@
         }
       //echo $str1Arr[$index];
     } 
+
+
 	$occPhp=strstr($string1,'PHP');              //find the occurance of string 'PHP'
 	if($occPhp){
  		echo("string PHP found <br/><br/>");
@@ -35,18 +37,21 @@
 	else{
  		echo("string PHP not found <br/><br/>");
 	}
+
+
     
     $findString="PHP";                            //find the position of string 'php' in string 1 
     $phpPos=strpos($string1,$findString);
     if($occPhp!==false){
-       /* echo "<div>\n";
-         echo "</div>"; */
+      
  		echo ("occurance of 'PHP' string is at position  $phpPos <br/>");
        
 	}
 	else{
  		echo("string PHP not found<br/>");
 	}
+
+
 
     $tok = strtok($string1, " ");               //break string and store in array and display using recursive function. 
     
@@ -65,5 +70,49 @@
      $string3=$string1.$string2;
      echo"<br/><br/> combine of string 1 and 2 is:<br/>";
      echo $string3; 
+
+     echo "</br></br> Today's date:<br/>";               //print particular date date.
+     echo date("Y/m/d");
+     
+     $pDate = mktime(0,0,0,1,12,2012);
+     echo "<br/><br/>Perticular date  is: ".date("Y/m/d", $pDate);
+
+     $add7Date=mktime(0,0,0,date("m"),date("d")+7,date("Y"));           //print date after seven days.
+     echo "<br/><br/>Date after seven days is: ".date("Y/m/d",$add7Date);
+      
+     $slen=strlen($string1);                                  //split string into 4 patrs;
+     $splitLength=$slen/4;
+     $spartArray=array();
+     $spartArray=str_split($string1,$splitLength);
+     echo "<br/>";
+     for ($i=0; $i<=3; $i++)
+     {
+       echo "<br/>The $i part is : $spartArray[$i]";
+     }
+       
+
+
+  /*  $stok = strtok($string1, ".");               /divide astring by occurenses of . 
+    $strDotArray=array();
+    $sindex=0;
+    while ($stok !== false) {
+    //echo "Word=$tok<br />";
+     $strDotArray[$sindex++]=$stok;
+     //$stok = strtok(".");
+    }
+    */
+    echo "<br/><br/> "; 
+
+     $stok = strtok($string1, " ");               //print the occuranse of PHP using string function 
+    
+    while ($stok !== false) {
+    //echo "Word=$tok<br />";
+     //$str1Arr[$index++]=$tok;
+      if((strcmp($stok,"PHP"))==0)
+      {
+          echo "<br/> string found: $stok ";
+       }
+     $stok = strtok(" \n\t");
+    }
 
 ?> 
